d4c9fed9b8fe5ed190f12979d02b4231cc12c1d5
Michael Jenkins <mtj4@pdx.edu>
2020-11-24 12:57:07 -0800

    Update Card.js
    replaced placeholder text

--- react-site/vibecheck/src/Card.js
+++ react-site/vibecheck/src/Card.js
@@ -16,12 +16,22 @@ class MainCard extends React.Component {
                         VibeCheck
                         </Card.Title>
                         <Card.Text>
-                            Vibecheck is our final project. Info about the project will be on this card.
+                            Vibecheck is our final project for CS 465P.
+                            <br/>
+                            It was created by Michael Jenkins, Nhan Le, and Jaeger Tang.
                             <br/><br/>
-                            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
-                            dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non 
-                            proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                            Consent is an often overlooked issue in our society, and accusations of harrassment and assault all too often go unheard,
+                            causing problematic people to continue unopposed. 
+                            <br/><br/>
+                            Vibecheck was created to play a small role in remedying this. If someone has made you feel unsafe or harmed you in some manner, 
+                            their basic information can be submitted to our database.
+                            <br/><br/>
+                            If you have concerns about an upcoming date or a new roommate, you can use their information to search our database and we'll let you
+                            know whether they appear in it and a general idea of how many reports there have been.
+                            <br/><br/>
+                            If you submit a report, we don't collect or store any information about you, it's fully anonymous for your protection.
+                            <br/>
+                            As a consequence of this, reports cannot be verified. 
                         </Card.Text>
                     </Card.Body>
                 </Card>
