91306f797f7e5661ba766d7d506156fdb02b2aa7
nhanlt81 <ntl2@pdx.edu>
2020-11-25 18:59:42 -0800

    Update Server.js
    Update a report with more fields

--- react-site/server/server.js
+++ react-site/server/server.js
@@ -23,11 +23,11 @@ app.use(
 app.use(parser.json());
 
 // Object Declaration 
-let Perp = (p_name,p_state, p_phone) => {
-    return {name: p_name, state : p_state, phone : p_phone  };
-}
-let Incident = (p_pid, p_state, p_year, p_details ) => {
-    return {perp_ID : p_pid, state: p_state, year: p_year, details : p_details };
+
+
+
+let Incident = (p_fname, p_lname, p_state, p_phone, p_year, p_work, p_school ) => {
+    return {fname : p_fname, lname: p_lname, state: p_state, phone: p_phone, year: p_year, work : p_work, school : p_school};
 };
 
 
@@ -63,43 +63,19 @@ mongodbClient.connect(mongodb_connection_string,{native_parser: true, useUnified
 
     });
 
-    // This is how I handle to test at Server side, if you get access from Client to this function you need to fix some steps here.
-    // first, at line 88, change get to post
-    // second,at line 94, {'name' : "Jane", 'phone' : "971-533-0000"} needs to get values by using req.body.perp get get (name, phone).So, it is required to fix
-    // last ,at line 110, "CA","2010", "Portland State University" needs to get values by using req.body.incident get get (state, year, details).So, it is required to fix
-    // OR REQUEST ME TO FIX THIS
-    // Lookup the perp and fill out an incident
-    app.get("/createincident", (req, res) => {
-        // look up the existing perp
-        // now I just assume that I have retrieved the perp
-        // it will be static since we are not done with frontend, so I will change this code
-        // to search a perp base on name and phone#
-
-        db.collection('Perp').findOne({'name' : "Jane", 'phone' : "971-533-0000"})
-                            .then((result) => {
-                                // check whether a student is in a list or not
-                                    if(!result){
-                                        // no perp exist
-                                        console.log("No Perp Found");
-                                        res.send("fail to create an incident due to no perp.");
-                                    }else{
-                                        // the perp existed
-                                        let perpId = result._id; // return perp id
-                                        
-                                        // Note: state gets from POST request
-                                        // year gets from POST request
-                                        // detail get from POST request 
-                                        // Assume I have incident detail like (perp_Id, state, year, details) as ("5fbdeab426780053b8022758","CA","2010","Portland State University")
-                                        let newIncident = Incident(perpId, "CA","2010", "Portland State University" );
-                                        db.collection('Incident').insertOne(newIncident, (err, res) => {
-                                            if(err) throw err;
-                                            console.log('one document inserted');
-                                        })
-                                        res.send("Insert an Incident related with Perp succesfully!!!");
-                                        
-                                    }
-                            })
+    
+    app.post("/createincident", (req, res) => {
+        console.log(req.body.incident);
+        var report = req.body.report;
+        let newIncident = Incident(report.fName, report.lName, report.state, report.phone, report.year, report.work, report.school);
+        db.collection('Incident').insertOne(newIncident, (err, res) => {
+            if(err) throw err;
+            console.log('one document inserted');
         })
+        res.send("Insert an Incident related with Perp succesfully!!!");
+        
+        
+    })
 
     // Search that entry whether it is existed in list or not
     // If not existed, insert that data to the database, send a successful message back to client
