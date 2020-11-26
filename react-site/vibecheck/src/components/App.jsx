8702e9fc78e177cf63d46d0a40040432db9690e2
Michael Jenkins <mtj4@pdx.edu>
2020-11-25 16:41:27 -0800

    restructured

---
+++ react-site/vibecheck/src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import MainCard from './Card';
+import NavigationBar from './Navbar';
+
+function App() {
+
+    return (
+    <>
+        <NavigationBar />
+        <MainCard />
+    </>
+)};
+
+export default App;
