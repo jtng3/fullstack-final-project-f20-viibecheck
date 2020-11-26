c8056eb797053c9bca447cc1c9f0d0b3a54d7689
nhanlt81 <ntl2@pdx.edu>
2020-11-25 17:02:28 -0800

    Update App.jsx

--- react-site/vibecheck/src/components/App.jsx
+++ react-site/vibecheck/src/components/App.jsx
@@ -1,7 +1,7 @@
-import React from "react";
-import "./App.css";
-import MainCard from "./Card";
-import NavigationBar from "./Navbar";
+import React from 'react';
+import './App.css';
+import MainCard from './Card';
+import NavigationBar from './Navbar';
 
 function App() {
   return (
