abf59134e12e25863306919d856f2d7457916fa7
jtng3 <tang3@pdx.edu>
2020-11-27 18:43:01 -0800

    implemented hidden checkbox with label as a button in Details.js, added Details.css

--- react-site/vibecheck/src/components/form-components/Details.jsx
+++ react-site/vibecheck/src/components/form-components/Details.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { Form } from "react-bootstrap";
 import detailTypes from "./detailTypes";
+import "./Details.css";
 
 function Details(props) {
-
   return (
     <Form.Group id="detailsContainer">
       <Form.Label htmlFor="location" id="stateLabel">
@@ -11,13 +11,23 @@ function Details(props) {
       </Form.Label>
       {detailTypes.map((detailType, index) => (
         <div key={index}>
+          {/*           <label>
+            <input
+              className="chk-btn mr-2"
+              type="checkbox"
+              name={detailType.id}
+              onChange={props.updateDetails}
+            />
+            {detailType.content}
+          </label> */}
           <input
-            className="mr-2"
             type="checkbox"
+            class="chk-btn"
+            id={detailType.id}
             name={detailType.id}
             onChange={props.updateDetails}
           />
-          <label>{detailType.content}</label>
+          <label for={detailType.id}>{detailType.content}</label>
         </div>
       ))}
     </Form.Group>
